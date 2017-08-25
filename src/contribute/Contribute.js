ec426e266a34335019b3b66ca457011db12bcd02
Nick Pierson <nick@nickpierson.me>
2017-08-25 11:40:14 -0500

    style section text in contribute section

--- src/contribute/Contribute.js
+++ src/contribute/Contribute.js
@@ -6,12 +6,12 @@ function Contribute(props) {
   return (
     <section id="contribute" className="section">
       <h2 className="section__title">Contribute</h2>
-      <p>
+      <p className="section__text">
         This website doesn't run for free. I appreciate anything you do to help!
         You can support me on Patreon, donate directly, or help translate the site.
       </p>
       <h3 className="section__subtitle">Patreon</h3>
-      <p>
+      <p className="section__text">
         I recently started a Patreon and I'm hoping for this to be the best way
         for me to get feedback from my users and to receive donations.
         I will try to host regular polls and engage with the community to see
@@ -23,14 +23,14 @@ function Contribute(props) {
         </a>
       </div>
       <h3 className="section__subtitle">Donate</h3>
-      <p>
+      <p className="section__text">
         If you want to help monetarily but you're not into that Patreon thing,
         I totally understand. I've opened up a Stripe account just for you —
         and you can donate as much or as little as you want.
       </p>
       <DonateForm />
       <h3 className="section__subtitle">Translate this Site</h3>
-      <p>
+      <p className="section__text">
         If you would like to see this site in a different language, please
         help me out by submitting your translations to me. I would love to see
         this site be translated into every language.
