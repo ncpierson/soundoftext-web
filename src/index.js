edc565e086d63dce8c49e1290a8fa248e375af2a
Nick Pierson <nick@nickpierson.me>
2017-08-27 16:40:59 -0500

    track pageviews with google analytics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import Home from './home/Home';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
+
+// Google Analytics
+ReactGA.initialize('UA-101624095-2');
+ReactGA.pageview('/');
