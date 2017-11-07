b0e6e357006268456214dde2ca6eed0200d92e6c
Nick Pierson <nick@nickpierson.me>
2017-11-06 19:03:43 -0600

    handle donation error if error is in stripe, not http request

--- src/contribute/DonateForm.js
+++ src/contribute/DonateForm.js
@@ -38,6 +38,8 @@ class DonateForm extends Component {
         return response.json();
       })
       .then(json => {
+        if (!json.success) throw new Error(json.error.message);
+
         this.setState({ hasDonated: true });
       })
       .catch(error => {
