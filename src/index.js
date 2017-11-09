719cfff690c01ad36862cc39ef5f0181ce8c7b6a
Nick Pierson <nick@nickpierson.me>
2017-11-09 16:46:20 -0600

    add array.find polyfill for ie11

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './polyfills';
 
 import './index.css';
 
