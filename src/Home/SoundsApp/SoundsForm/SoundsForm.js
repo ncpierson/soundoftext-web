83768f6ef3a1d715a27263c5c13cf834711392e0
Nick Pierson <nick@nickpierson.me>
2018-01-01 13:29:37 -0600

    remove welcome message

--- src/Home/SoundsApp/SoundsForm/SoundsForm.js
+++ src/Home/SoundsApp/SoundsForm/SoundsForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { HotKeys } from 'react-hotkeys';
-import { OutboundLink } from 'react-ga';
 import VoiceOptions from './VoiceOptions';
 import './SoundsForm.css';
 
@@ -8,19 +7,14 @@ class SoundsForm extends Component {
   constructor() {
     super();
 
-    let isWelcome = window.localStorage.getItem('isWelcome');
-    isWelcome = isWelcome === null ? true : isWelcome === 'true';
-
     this.state = {
       text: '',
-      voice: 'en-US',
-      isWelcome: isWelcome
+      voice: 'en-US'
     };
 
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleVoiceChange = this.handleVoiceChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleWelcomeDismiss = this.handleWelcomeDismiss.bind(this);
   }
 
   handleTextChange(e) {
@@ -37,11 +31,6 @@ class SoundsForm extends Component {
     this.setState({ text: '' });
   }
 
-  handleWelcomeDismiss(e) {
-    window.localStorage.setItem('isWelcome', false);
-    this.setState({ isWelcome: false });
-  }
-
   render() {
     const keyMap = {
       submit: ['ctrl+enter', 'command+enter']
@@ -54,20 +43,6 @@ class SoundsForm extends Component {
     return (
       <div className="section">
         <div className="grid">
-          {this.state.isWelcome && (
-            <div className="grid__item grid__item--thin welcome">
-              <p className="section__text">
-                Having trouble with the new site? You can still access the old
-                one at{' '}
-                <OutboundLink to="http://old.soundoftext.com" eventLabel="old">
-                  old.soundoftext.com
-                </OutboundLink>, but it will only be up for a couple of months.
-              </p>
-              <div className="welcome__dismiss">
-                <a onClick={this.handleWelcomeDismiss}>Dismiss</a>
-              </div>
-            </div>
-          )}
           <form
             className="grid__item grid__item--thin"
             onSubmit={this.handleSubmit}
