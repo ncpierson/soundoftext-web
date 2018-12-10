2950f10fb40f810bff76a41f7e91df253055bd99
Nick Pierson <me@nick.exposed>
2018-12-10 01:27:17 +0000

    fail ci if docker login fails

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+set -x
+
+if [ "$CI" ]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+fi
+
 USERNAME=ncpierson
 IMAGE=soundoftext-web
 VERSION=`cat VERSION`
 
 ./scripts/build.sh
-
-if [ "$CI" ]; then
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-fi
 
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$VERSION
 docker push $USERNAME/$IMAGE:latest
