e062528498803941f0a715ce480754186dc693d4
Nick Pierson <me@nick.exposed>
2021-04-20 05:17:51 +0000

    remove 'translate this site' section

--- web/src/Home/Contribute/Contribute.js
+++ web/src/Home/Contribute/Contribute.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { OutboundLink } from 'react-ga';
-import { Link } from 'react-router-dom';
 import DonateForm from './DonateForm';
 import './Contribute.css';
 
@@ -33,11 +32,6 @@ function Contribute(props) {
               Become a Patron
             </OutboundLink>
           </div>
-          <div className="grid__item grid__item--thin">
-            <Link to="/supporters" className="button button--link">
-              The Patrons
-            </Link>
-          </div>
         </div>
       </div>
       <div className="section">
@@ -50,16 +44,6 @@ function Contribute(props) {
           </p>
           <DonateForm />
         </div>
-      </div>
-      <div className="section section--bordered">
-        <h3 className="section__subtitle">Translate this Site</h3>
-        <p className="section__text">
-          If you would like to see this site in a different language, please
-          help me out by submitting your translations to me. I would love to see
-          this site be translated into every language. Just send an email to{' '}
-          <a href="mailto:contact@soundoftext.com">contact@soundoftext.com</a>{' '}
-          and say that you want to help translate the site.
-        </p>
       </div>
     </section>
   );
